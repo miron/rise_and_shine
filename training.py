17632123da947792c0195826d79c5244ab2bef22
Miron <miron@users.noreply.github.com>
2023-01-27 17:04:06 +0100

    Create training.py

---
+++ training.py
@@ -0,0 +1,19 @@
+# Rise & Shine Training Card
+training_card = [
+    # Warm-up
+    {'Exercise': 'Hyperventilation', 'Reps': 30, 'Sets': 9},
+    # Strength Training
+    {'Exercise': 'Push-ups', 'Reps': 36, 'Sets': 3},
+    {'Exercise': 'Pull-ups', 'Reps': 10, 'Sets': 3},
+    {'Exercise': 'Dips', 'Reps': 20, 'Sets': 3},
+    {'Exercise': 'Squats', 'Reps': 20, 'Sets': 3},
+    {'Exercise': 'Handstand push-ups', 'Reps': 3, 'Sets': 3},
+    {'Exercise': 'Bridge push-ups', 'Reps': 10, 'Sets': 3},
+    {'Exercise': 'Crunches', 'Reps': 50, 'Sets': 3},
+    # Cardio
+    {'Exercise': 'Run', 'Distance': 4, 'Time': 30},
+    # Yoga
+    {'Exercise': 'Half Primary Series with Laruga Glaser', 'Time': 65},
+    # Meditation
+    {'Exercise': 'Meditation with Swami Rama', 'Time': 10}
+]
